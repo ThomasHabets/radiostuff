7eb077cf4bcb70c25cf6a91cdf6dfc5ad8b52009
Thomas Habets <thomas@habets.se>
2017-08-20 19:08:47 +0100

    tools/rcv: Print text message

--- tools/rcv.py
+++ tools/rcv.py
@@ -6,9 +6,9 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 12345)
+server_address = ('localhost', 52002)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 while True:
     data, address = sock.recvfrom(4096)
-    print ['%02x' % ord(x) for x in data]
+    print ['%02x' % ord(x) for x in data], data

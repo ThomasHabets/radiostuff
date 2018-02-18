d123704fe433d47260aa66a953caaf2ca639602b
takurx <taku3wzr@gmail.com>
2018-02-18 16:24:42 +0900

    add library definition
    I add library definition, iostream, fstream.
    For make command, file_pdu_generator_impl.cc is error that
    'std::ifstream ifs' has initializer but incomplete type......
    My environment that Ubuntu 16.04 LTS needed it.

--- gr-habets/lib/file_pdu_generator_impl.h
+++ gr-habets/lib/file_pdu_generator_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2017 Thomas Habets <thomas@habets.se>
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -21,8 +21,10 @@
 #ifndef INCLUDED_HABETS_FILE_PDU_GENERATOR_IMPL_H
 #define INCLUDED_HABETS_FILE_PDU_GENERATOR_IMPL_H
 
-#include<string>
+#include <string>
 #include <habets/file_pdu_generator.h>
+#include <iostream>
+#include <fstream>
 
 namespace gr {
   namespace habets {
@@ -49,4 +51,3 @@ namespace gr {
 } // namespace gr
 
 #endif /* INCLUDED_HABETS_FILE_PDU_GENERATOR_IMPL_H */
-

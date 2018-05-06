e1095842dda489ca47ddab40b4507804b558652f
Thomas Habets <thomas@habets.se>
2018-05-06 12:10:33 +0100

    wxlog: Log dial and mode

--- wxlog/wxlog.go
+++ wxlog/wxlog.go
@@ -50,16 +50,38 @@ type Packet struct {
 	SNR            int32
 	Delta          float64
 	DeltaFrequency uint32
-	Mode           string
+	DecodeMode     string
 	Message        string
 	LowConfidence  bool
 	OffAir         bool
+
+	// Status
+	// ID string
+	Dial         uint64
+	Mode         string
+	DXCall       string
+	Report       string
+	TXMode       string
+	TXEnabled    bool
+	Transmitting bool
+	Decoding     bool
+	RXDF         int32
+	TXDF         int32
+	DECall       string
+	DEGrid       string
+	DXGrid       string
+	TXWatchdog   bool
+	SubMode      string
+	FastMode     bool
 }
 
 func readString(r io.Reader) (string, error) {
 	var l uint32
 	if err := binary.Read(r, binary.BigEndian, &l); err != nil {
 		return "", err
+	}
+	if l == 4294967295 {
+		return "", nil
 	}
 	b := make([]byte, l, l)
 	n, err := r.Read(b)
@@ -82,80 +104,170 @@ func readTime(r io.Reader) (time.Time, error) {
 	return time.Unix(n-(n%86400)+int64(t)/1000, int64(t)%1000), err
 }
 
-func printPacket(p []byte) error {
+func readStatusPacket(buf *bytes.Buffer, pp *Packet) error {
+	var err error
+
+	pp.ID, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading ID: %v", err)
+	}
+
+	if err := binary.Read(buf, binary.BigEndian, &pp.Dial); err != nil {
+		return err
+	}
+
+	pp.Mode, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading Mode: %v", err)
+	}
+	pp.DXCall, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading DXCall: %v", err)
+	}
+	pp.Report, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading Report: %v", err)
+	}
+	pp.TXMode, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading TXMode: %v", err)
+	}
+
+	pp.TXEnabled, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	pp.Transmitting, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	pp.Decoding, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &pp.RXDF); err != nil {
+		return err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &pp.TXDF); err != nil {
+		return err
+	}
+	pp.DECall, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading DECall: %v", err)
+	}
+	pp.DEGrid, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading DEGrid: %v", err)
+		return err
+	}
+	pp.DXGrid, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading DXGrid: %v", err)
+		return err
+	}
+	pp.TXWatchdog, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	pp.SubMode, err = readString(buf)
+	if err != nil {
+		return fmt.Errorf("reading SubMode: %v", err)
+		return err
+	}
+	pp.FastMode, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func readDecodePacket(buf io.Reader, pp *Packet) error {
+	var err error
+
+	pp.ID, err = readString(buf)
+	if err != nil {
+		return err
+	}
+	pp.New, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	pp.Time, err = readTime(buf)
+	if err != nil {
+		return err
+	}
+
+	if err := binary.Read(buf, binary.BigEndian, &pp.SNR); err != nil {
+		return err
+	}
+
+	if err := binary.Read(buf, binary.BigEndian, &pp.Delta); err != nil {
+		return err
+	}
+
+	if err := binary.Read(buf, binary.BigEndian, &pp.DeltaFrequency); err != nil {
+		return err
+	}
+
+	pp.DecodeMode, err = readString(buf)
+	if err != nil {
+		return err
+	}
+	pp.Message, err = readString(buf)
+	if err != nil {
+		return err
+	}
+
+	pp.LowConfidence, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	pp.OffAir, err = readBool(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func decodePacket(p []byte) (*Packet, error) {
 	var pp Packet
 	buf := bytes.NewBuffer(p)
 
 	if err := binary.Read(buf, binary.BigEndian, &pp.Magic); err != nil {
-		return err
+		return nil, err
 	}
 	if pp.Magic != magic {
-		return fmt.Errorf("incorrect magic %v", pp.Magic)
+		return nil, fmt.Errorf("incorrect magic %v", pp.Magic)
 	}
 
 	if err := binary.Read(buf, binary.BigEndian, &pp.Schema); err != nil {
-		return err
+		return nil, err
 	}
 	if pp.Schema != schema {
-		return fmt.Errorf("incorrect schema %v", pp.Schema)
+		return nil, fmt.Errorf("incorrect schema %v", pp.Schema)
 	}
 
 	if err := binary.Read(buf, binary.BigEndian, &pp.Type); err != nil {
-		return err
+		return nil, err
 	}
 	switch pp.Type {
 	case PacketTypeDecode:
-		var err error
-
-		pp.ID, err = readString(buf)
-		if err != nil {
-			return err
-		}
-		pp.New, err = readBool(buf)
-		if err != nil {
-			return err
-		}
-		pp.Time, err = readTime(buf)
-		if err != nil {
-			return err
-		}
-
-		if err := binary.Read(buf, binary.BigEndian, &pp.SNR); err != nil {
-			return err
-		}
-
-		if err := binary.Read(buf, binary.BigEndian, &pp.Delta); err != nil {
-			return err
-		}
-
-		if err := binary.Read(buf, binary.BigEndian, &pp.DeltaFrequency); err != nil {
-			return err
-		}
-
-		pp.Mode, err = readString(buf)
-		if err != nil {
-			return err
-		}
-		pp.Message, err = readString(buf)
-		if err != nil {
-			return err
-		}
-
-		pp.LowConfidence, err = readBool(buf)
-		if err != nil {
-			return err
-		}
-		pp.OffAir, err = readBool(buf)
-		if err != nil {
-			return err
+		if err := readDecodePacket(buf, &pp); err != nil {
+			return nil, err
 		}
 		if buf.Len() > 0 {
 			log.Errorf("Bytes left in Decode packet: %v", buf.Bytes())
 		}
-		fmt.Printf("%v %4d %4.1f %4d %s\n", pp.Time.UTC().Format("2006-01-02 15:04:05Z"), pp.SNR, pp.Delta, pp.DeltaFrequency, pp.Message)
+	case PacketTypeStatus:
+		if err := readStatusPacket(buf, &pp); err != nil {
+			return nil, err
+		}
+		if buf.Len() > 0 {
+			log.Errorf("Bytes left in Status packet: %v", buf.Bytes())
+		}
 	}
 
-	return nil
+	return &pp, nil
 }
 
 func main() {
@@ -170,7 +282,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to listen to %#v: %v", a)
 	}
-	fmt.Printf("%20s %4s %4s %4s %s\n", "Time", "SNR", "DT", "Freq", "Message")
+	fmt.Printf("%20s %10s %6s %4s %4s %4s %s\n", "Time", "Dial", "Mode", "SNR", "DT", "Freq", "Message")
+	mode := "???"
+	var freq uint64
 	for {
 		b := make([]byte, 1500, 1500)
 		oob := make([]byte, 1500, 1500)
@@ -185,8 +299,16 @@ func main() {
 		remote = remote
 		fl = fl
 		p := b[:n]
-		if err := printPacket(p); err != nil {
+		pp, err := decodePacket(p)
+		if err != nil {
 			log.Error(err)
+		}
+		switch pp.Type {
+		case PacketTypeStatus:
+			mode = pp.Mode
+			freq = pp.Dial
+		case PacketTypeDecode:
+			fmt.Printf("%v %10d %6s %4d %4.1f %4d %s\n", pp.Time.UTC().Format("2006-01-02 15:04:05Z"), freq, mode, pp.SNR, pp.Delta, pp.DeltaFrequency, pp.Message)
 		}
 	}
 }

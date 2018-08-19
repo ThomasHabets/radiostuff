0b3a6dd02bf7a2fbd0da41e159a8b39ae7bf6a56
Thomas Habets <thomas@habets.se>
2018-08-19 16:39:23 +0100

    gr-habets/magic decoder: Also send symbols

--- gr-habets/python/magic_decoder.py
+++ gr-habets/python/magic_decoder.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2017 Thomas Habets <thomas@habets.se>
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import numpy
 
@@ -25,8 +25,8 @@ from gnuradio import gr
 from gnuradio.gr import pmt
 
 from wpcr import wpcr, slice_bits
-            
-                        
+
+
 class magic_decoder(gr.basic_block):
     """
     docstring for block magic_decoder
@@ -37,13 +37,15 @@ class magic_decoder(gr.basic_block):
             in_sig=[],
             out_sig=[])
         self.port_out = pmt.intern("out")
+        self.port_out_symbols = pmt.intern("symbols")
         self.port_in = pmt.intern("in")
         self.port_info = pmt.intern("info")
         self.message_port_register_out(self.port_out)
+        self.message_port_register_out(self.port_out_symbols)
         self.message_port_register_out(self.port_info)
         self.message_port_register_in(self.port_in)
         self.set_msg_handler(self.port_in, self._handle_msg)
-       
+
     def _handle_msg(self, msg_pmt):
         meta = pmt.car(msg_pmt)
         bs = pmt.u8vector_elements(pmt.cdr(msg_pmt))
@@ -51,11 +53,18 @@ class magic_decoder(gr.basic_block):
         #print "Got msg of size %d" % len(fs)
         #print midpoint(fs)
         misc, data = wpcr(fs)
+        meta = pmt.dict_add(meta, pmt.intern("magic_decoder"), pmt.to_pmt(misc))
+        self.message_port_pub(self.port_info, meta)
+
+        # Send symbols.
+        #vec = pmt.init_f32vector(len(data), data)
+        vec = pmt.to_pmt([float(x) for x in data])
+        self.message_port_pub(self.port_out_symbols, pmt.cons(meta, vec))
+
+        # Send bits.
         bits = slice_bits(data)
         vec = pmt.init_u8vector(len(bits), [int(x) for x in bits])
-        meta = pmt.dict_add(meta, pmt.intern("magic_decoder"), pmt.to_pmt(misc))
         self.message_port_pub(self.port_out, pmt.cons(meta, vec))
-        self.message_port_pub(self.port_info, meta)
 
     def forecast(self, noutput_items, ninput_items_required):
         #setup size of input_items[i] for work call

44bee01cf6a7e4769fb4a974822261b46dc7ff4c
Thomas Habets <thomas@habets.se>
2018-09-09 14:32:16 +0100

    simplex-repeater: Tweak to work on raspberry pi zero

--- simplex-repeater/rpt.py
+++ simplex-repeater/rpt.py
@@ -9,9 +9,15 @@ audioformat=audio.PCM_FORMAT_S16_LE
 # second arg to PCM can be ,audio.PCM_NONBLOCK
 periodsize=1024
 rate=44100
+indev='hw:1,0'
+outdev='hw:1,0'
+skip=3
 
 def fix(d):
     """Turn mono data into stereo"""
+    line=d
+    n=2
+    return ''.join([line[i:i+n]*2 for i in range(0, len(line), n)])
     shorts = struct.unpack('<' + 'h' * (len(d)/2), d)
     dbl = reduce(lambda x,y: x+y, zip(shorts, shorts))
     return struct.pack('<' + 'h' * len(d), *dbl)
@@ -20,10 +26,12 @@ def record(inp):
     triggered = False
     alldata = []
     silent = 0
-    # Skip 2 seconds.
-    print '... skipping 2s'
-    for _ in range(2 * int(float(rate)/periodsize)):
+
+    # Skip a bit before starting.
+    print '... skipping %ds' % (skip)
+    for _ in range(int(skip*float(rate)/periodsize)):
         inp.read()
+
     print '... ok, listening'
     while True:
         l,data = inp.read()
@@ -44,17 +52,17 @@ def record(inp):
         if silent > 100:
             return alldata
 
-        alldata.append(data)
+        alldata.append(fix(data))
 
 
 def main():
-    inp = audio.PCM(audio.PCM_CAPTURE,card='hw:2,0')
+    inp = audio.PCM(audio.PCM_CAPTURE,card=indev)
     inp.setchannels(1)
     inp.setrate(rate)
     #inp.setformat(audioformat)
     inp.setperiodsize(periodsize)
 
-    out = audio.PCM(audio.PCM_PLAYBACK,card='hw:1,0')
+    out = audio.PCM(audio.PCM_PLAYBACK,card=outdev)
     out.setchannels(2)
     out.setrate(rate)
     #out.setformat(audioformat)
@@ -65,11 +73,22 @@ def main():
         st = time.time()
         rec = record(inp)
         print '... took %ds and gave %d data' % (time.time()-st, len(rec))
+        print '... %d data should be %.2fs' % (len(rec), len(rec)*periodsize/44100)
 
+        if False:
+            st = time.time()
+            fixed = []
+            print 'transforming...'
+            for piece in rec:
+                fixed.append(fix(piece))
+            print '... took', (time.time()-st)
+        else:
+            fixed = rec    
+            
         st = time.time()
         print "playback..."
-        for piece in rec:
-            out.write(fix(piece))
+        for piece in fixed:
+            out.write(piece)
         print '... took', (time.time()-st)
 
 main()

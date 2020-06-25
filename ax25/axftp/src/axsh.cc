1e7623f69342583612749e806a4c96d6f4c2a98e
Thomas Habets <thomas@habets.se>
2020-06-25 20:04:21 +0100

    ax25/axftp: Create basic authenticated shell

---
+++ ax25/axftp/src/axsh.cc
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+
+#include "axlib.h"
+
+using namespace axlib;
+
+namespace {
+void usage(const char* av0, int err)
+{
+    FILE* f = stdout;
+    if (err) {
+        f = stderr;
+    }
+    fprintf(f,
+            "Usage: %s [-h] [-eU] [-w <window>] -s <src call> [-p <digipath>] <dst>\n",
+            av0);
+    exit(err);
+}
+} // namespace
+
+int main(int argc, char** argv)
+{
+    CommonOpts copt;
+    int opt;
+    while ((opt = getopt(argc, argv, "k:P:ehl:p:s:w:")) != -1) {
+        if (common_opt(copt, opt)) {
+            continue;
+        }
+        switch (opt) {
+        case 'h':
+            usage(argv[0], EXIT_SUCCESS);
+        default: /* '?' */
+            usage(argv[0], EXIT_FAILURE);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Need dest\n");
+        exit(EXIT_FAILURE);
+    }
+    const std::string dst = argv[optind];
+
+    auto sock = make_from_commonopts(copt);
+
+    std::clog << "Connecting...\n";
+    if (sock->connect(dst)) {
+        std::clog << "Failed to connect!\n";
+        return 1;
+    }
+    std::clog << "Connected!\n";
+
+    std::mutex m;
+    std::string cmd;
+    std::thread reader([&] {
+        for (;;) {
+            {
+                std::unique_lock<std::mutex> l(m);
+                if (!cmd.empty()) {
+                    continue;
+                }
+            }
+
+            std::cout << "prompt\n";
+            std::vector<char> buf(128);
+            std::cin.getline(&buf[0], sizeof(buf));
+            const auto len = std::cin.gcount();
+            const auto line = std::string(&buf[0], &buf[len - 1]);
+            std::clog << "writing\n";
+
+            std::unique_lock<std::mutex> l(m);
+            cmd = line;
+        }
+    });
+    for (;;) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(sock->get_fd(), &fds);
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;
+        const auto rc = select(sock->get_fd() + 1, &fds, NULL, NULL, &tv);
+        {
+            std::unique_lock<std::mutex> l(m);
+            if (!cmd.empty()) {
+                sock->write(cmd);
+                cmd = "";
+            }
+        }
+        if (rc == 1) {
+            std::cout << sock->read() << std::flush;
+        }
+    }
+    reader.join();
+    return 0;
+}
---
+++ ax25/axftp/src/axshd.cc
@@ -0,0 +1,219 @@
+#include "axlib.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <functional>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+using namespace axlib;
+
+namespace {
+
+void xdup2(int old, int newfd)
+{
+    if (-1 == dup2(old, newfd)) {
+        throw std::runtime_error(std::string("dup2(): ") + strerror(errno));
+    }
+}
+
+class Pipe
+{
+public:
+    Pipe()
+    {
+        int p[2];
+        if (-1 == pipe(p)) {
+            throw std::runtime_error(std::string("pipe(): ") + strerror(errno));
+        }
+        r = p[0];
+        w = p[1];
+    }
+    ~Pipe() { close(); }
+    void close()
+    {
+        close_read();
+        close_write();
+    }
+    void close_read()
+    {
+        if (r != -1) {
+            ::close(r);
+            r = -1;
+        }
+    }
+    void close_write()
+    {
+
+        if (w != -1) {
+            ::close(w);
+            w = -1;
+        }
+    }
+
+    void write(const std::string& s)
+    {
+        const char* p = s.data();
+        size_t left = s.size();
+        for (;;) {
+            const auto rc = ::write(w, p, left);
+            if (rc == -1) {
+                throw std::runtime_error(std::string("pipe.write(): ") + strerror(errno));
+            }
+            left -= rc;
+            p += rc;
+            if (left == 0) {
+                return;
+            }
+        }
+    }
+
+    // read until eof
+    std::string read()
+    {
+        std::string ret;
+        for (;;) {
+            char buf[128];
+            const auto rc = ::read(r, buf, sizeof(buf));
+            if (rc == -1) {
+                throw std::runtime_error(std::string("pipe.read(): ") + strerror(errno));
+            }
+            if (rc == 0) {
+                return ret;
+            }
+            ret += std::string(&buf[0], &buf[rc]);
+        }
+    }
+
+    // No copy.
+    Pipe(const Pipe&) = delete;
+    Pipe& operator=(const Pipe&) = delete;
+
+    int r, w;
+};
+
+void shellout(const std::string& cmd, SeqPacket* conn)
+{
+    Pipe i;
+    Pipe o;
+    const pid_t pid = fork();
+    if (pid == -1) {
+        throw std::runtime_error(std::string("fork(): ") + strerror(errno));
+    }
+
+    if (pid == 0) {
+        xdup2(i.r, 0);
+        xdup2(o.w, 1);
+        xdup2(o.w, 2);
+        i.close();
+        o.close();
+        execlp("/bin/sh", "sh", "-c", cmd.c_str(), NULL);
+        fprintf(stderr, "Failed to exec: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    i.close_read();
+    o.close_write();
+
+    std::thread th([&] {
+        try {
+            // i.write(in);
+            // TODO: allow input
+            i.close_write();
+        } catch (const std::exception& e) {
+            std::clog << "Write to shellout failed: " << e.what();
+        }
+    });
+    for (;;) {
+        auto out = o.read();
+        if (out.empty()) {
+            break;
+        }
+        const int step = conn->max_packet_size();
+        for (int pos = 0; pos < out.size(); pos += step) {
+            conn->write(out.substr(pos, step));
+        }
+    }
+    th.join();
+    // TODO: waitpid and check return code.
+    int status;
+    if (-1 == waitpid(pid, &status, 0)) {
+        throw std::runtime_error(std::string("waitpid(): ") + strerror(errno));
+    }
+    if (WIFEXITED(status)) {
+        conn->write(">>> Exit status " + std::to_string(WEXITSTATUS(status)) + "\n");
+    } else if (WIFSIGNALED(status)) {
+        conn->write(">>> killed by signal: " + std::to_string(WTERMSIG(status)) + "\n");
+    } else {
+        conn->write(">>> died unknown status\n");
+    }
+}
+
+
+void handle2(std::unique_ptr<SeqPacket> conn)
+{
+    for (;;) {
+        const auto cmd = conn->read();
+        shellout(cmd, conn.get());
+    }
+}
+
+void handle(std::unique_ptr<SeqPacket> conn)
+{
+    try {
+        handle2(std::move(conn));
+    } catch (const std::exception& e) {
+        std::cerr << "Exception in client handler: " << e.what() << std::endl;
+    }
+}
+
+void usage(const char* av0, int err)
+{
+    FILE* f = stdout;
+    if (err) {
+        f = stderr;
+    }
+    fprintf(f, "Usage: %s [-h] [-eU] [-w <window> ] -s <my call> [-p <digipath>]\n", av0);
+    exit(err);
+}
+
+} // namespace
+
+int main(int argc, char** argv)
+{
+    CommonOpts copt;
+    int opt;
+    while ((opt = getopt(argc, argv, "ehk:P:l:p:s:w:")) != -1) {
+        if (common_opt(copt, opt)) {
+            continue;
+        }
+        switch (opt) {
+        case 'h':
+            usage(argv[0], EXIT_SUCCESS);
+        default: /* '?' */
+            usage(argv[0], EXIT_FAILURE);
+        }
+    }
+
+    if (optind != argc) {
+        fprintf(stderr, "Extra args on command line\n");
+        exit(EXIT_FAILURE);
+    }
+
+    auto sock = make_from_commonopts(copt);
+
+    std::clog << "Listening...\n";
+    sock->listen([](std::unique_ptr<SeqPacket> conn) {
+        std::clog << "Connection from " << conn->peer_addr() << "\n";
+        std::thread th(handle, std::move(conn));
+        th.detach();
+    });
+
+    return 0;
+}

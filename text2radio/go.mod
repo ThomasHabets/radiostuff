04bb3ba865c2f89801fe6e50eb0bd97180980029
Thomas Habets <thomas@habets.se>
2020-07-03 10:25:47 +0100

    Add GCP text2speech CLI

---
+++ text2radio/go.mod
@@ -0,0 +1,9 @@
+module github.com/ThomasHabets/radiostuff/text2radio
+
+go 1.14
+
+require (
+	cloud.google.com/go v0.60.0
+	github.com/sirupsen/logrus v1.6.0
+	google.golang.org/genproto v0.0.0-20200626011028-ee7919e894b5
+)
---
+++ text2radio/text2radio.go
@@ -0,0 +1,58 @@
+// Simple speech to text, writing oggs to stdout.
+package main
+
+// export GOOGLE_APPLICATION_CREDENTIALS=auth.json
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"io"
+	"os"
+
+	"cloud.google.com/go/texttospeech/apiv1"
+	log "github.com/sirupsen/logrus"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		log.Fatalf("Need to give the text")
+	}
+
+	ctx := context.Background()
+	log.Infof("Connecting…")
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	req := &texttospeechpb.SynthesizeSpeechRequest{
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{
+				Text: flag.Arg(0),
+			},
+		},
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "en-US",
+			// Name:
+			// SsmlGender:
+		},
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_OGG_OPUS,
+		},
+	}
+	log.Infof("Text2Speeching…")
+	resp, err := client.SynthesizeSpeech(ctx, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Infof("Writing…")
+	if _, err := io.Copy(os.Stdout, bytes.NewBuffer(resp.AudioContent)); err != nil {
+		log.Fatal(err)
+	}
+}
